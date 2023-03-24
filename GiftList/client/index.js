d12f729d598794fdeee7eeae7c68e54b6ff8fffa
Atharva <katharva01@gmail.com>
2023-03-24 19:14:05 +0530

    project 1,2 done

---
+++ GiftList/client/index.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+const verifyProof = require('../utils/verifyProof')
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+  // TODO: how do we prove to the server we're on the nice list? 
+  const name = "Robin Hessel Jr."
+  const mTree= new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n==name)
+  const proof = mTree.getProof(index);
+
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    // TODO: add request body parameters here!
+    name,
+    proof
+  });
+
+  console.log({ gift });
+}
+
+main();
